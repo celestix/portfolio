bb4b413165d0efc2926e7416f0cbf8a9fa2b1a38
celestix <i@anodite.me>
2024-06-27 01:24:21 +0530

    optimize project cards for smaller screens

---
+++ src/components/cards/ProjectCard.jsx
@@ -0,0 +1,16 @@
+export default function ProjectCard({name,description,link}) {
+    return (
+        <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+            <a href={link}>
+                <h5 class="mb-2 text-2xl font-semibold tracking-tight text-center pb-3">{name}</h5>
+            </a>
+            <p class="mb-3 font-normal text-gray-400 h-64 md:h-40">{description}</p>
+            <a href={link} class="inline-flex font-medium items-center text-blue-600 hover:underline">
+                Checkout now
+                <svg class="w-3 h-3 ms-2.5 rtl:rotate-[270deg]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11v4.833A1.166 1.166 0 0 1 13.833 17H2.167A1.167 1.167 0 0 1 1 15.833V4.167A1.166 1.166 0 0 1 2.167 3h4.618m4.447-2H17v5.768M9.111 8.889l7.778-7.778"/>
+                </svg>
+            </a>
+        </div>
+    )
+}
--- src/sections/projects/Projects.jsx
+++ src/sections/projects/Projects.jsx
@@ -1,94 +1,46 @@
+import ProjectCard from "../../components/cards/ProjectCard";
+
 export default function Projects() {
     return (
         <div class="px-20 pb-10">
           <h1 class="text-6xl font-extrabold text-center pb-14">Projects</h1>
           <br />
-          <div class="mx-auto grid grid-cols-4 gap-10 items-center justify-center">
-            <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-              <a href="https://t.me/GalactronBot">
-                  <h5 class="mb-2 text-2xl font-semibold tracking-tight text-center pb-3">Galactron</h5>
-              </a>
-              <p class="mb-3 font-normal text-gray-400 h-40">A powerful Telegram bot to help people with channel management, written in Go.</p>
-              <a href="https://t.me/GalactronBot" class="inline-flex font-medium items-center text-blue-600 hover:underline">
-                  Checkout now
-                  <svg class="w-3 h-3 ms-2.5 rtl:rotate-[270deg]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                      <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11v4.833A1.166 1.166 0 0 1 13.833 17H2.167A1.167 1.167 0 0 1 1 15.833V4.167A1.166 1.166 0 0 1 2.167 3h4.618m4.447-2H17v5.768M9.111 8.889l7.778-7.778"/>
-                  </svg>
-              </a>
-            </div>
-            <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-              <a href="https://github.com/celestix/gotgproto">
-                  <h5 class="mb-2 text-2xl font-semibold tracking-tight text-center pb-3">GoTGProto</h5>
-              </a>
-              <p class="mb-3 font-normal text-gray-400 h-40">A package that aims to make td's raw functions easy-to-use with the help of features like using session strings, custom helper functions, storing peers and extracting chat or user ids through it etc. It is written in Go.
-              </p>
-              <a href="https://github.com/celestix/gotgproto" class="inline-flex font-medium items-center text-blue-600 hover:underline">
-                  Checkout now
-                  <svg class="w-3 h-3 ms-2.5 rtl:rotate-[270deg]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                      <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11v4.833A1.166 1.166 0 0 1 13.833 17H2.167A1.167 1.167 0 0 1 1 15.833V4.167A1.166 1.166 0 0 1 2.167 3h4.618m4.447-2H17v5.768M9.111 8.889l7.778-7.778"/>
-                  </svg>
-              </a>
-            </div>
-            <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-              <a href="https://github.com/warpdl/warpdl">
-                  <h5 class="mb-2 text-2xl font-semibold tracking-tight text-center pb-3">WarpDL</h5>
-              </a>
-              <p class="mb-3 font-normal text-gray-400 h-40">WarpDL is a powerful and versatile cross-platform download manager. With its advanced technology, It has the ability to accelerate your download speeds by up to 10 times, revolutionizing the way you obtain files on any operating system.</p>
-              <a href="https://github.com/warpdl/warpdl" class="inline-flex font-medium items-center text-blue-600 hover:underline">
-                  Checkout now
-                  <svg class="w-3 h-3 ms-2.5 rtl:rotate-[270deg]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                      <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11v4.833A1.166 1.166 0 0 1 13.833 17H2.167A1.167 1.167 0 0 1 1 15.833V4.167A1.166 1.166 0 0 1 2.167 3h4.618m4.447-2H17v5.768M9.111 8.889l7.778-7.778"/>
-                  </svg>
-              </a>
-            </div>
-            <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-              <a href="https://github.com/celestix/agroconnect">
-                  <h5 class="mb-2 text-2xl font-semibold tracking-tight text-center pb-3">AgroConnect</h5>
-              </a>
-              <p class="mb-3 font-normal text-gray-400 h-40">AgroConnect is a one-place app that provides direct-to-consumer services such as buying and selling crops, cutting out the middleman, and more.</p>
-              <a href="https://github.com/celestix/agroconnect" class="inline-flex font-medium items-center text-blue-600 hover:underline">
-                  Checkout now
-                  <svg class="w-3 h-3 ms-2.5 rtl:rotate-[270deg]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                      <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11v4.833A1.166 1.166 0 0 1 13.833 17H2.167A1.167 1.167 0 0 1 1 15.833V4.167A1.166 1.166 0 0 1 2.167 3h4.618m4.447-2H17v5.768M9.111 8.889l7.778-7.778"/>
-                  </svg>
-              </a>
-            </div>
-            <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-              <a href="https://github.com/ferrisgram/ferrisgram">
-                  <h5 class="mb-2 text-2xl font-semibold tracking-tight text-center pb-3">Ferrisgram</h5>
-              </a>
-              <p class="mb-3 font-normal text-gray-400 h-40">FerrisGram is an asynchronous autogenerated wrapper for the Telegram Bot API written in Rust. This library provides all the methods and types that are available on the official Telegram Bot API.</p>
-              <a href="https://github.com/ferrisgram/ferrisgram" class="inline-flex font-medium items-center text-blue-600 hover:underline">
-                  Checkout now
-                  <svg class="w-3 h-3 ms-2.5 rtl:rotate-[270deg]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                      <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11v4.833A1.166 1.166 0 0 1 13.833 17H2.167A1.167 1.167 0 0 1 1 15.833V4.167A1.166 1.166 0 0 1 2.167 3h4.618m4.447-2H17v5.768M9.111 8.889l7.778-7.778"/>
-                  </svg>
-              </a>
-            </div>
-            <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-              <a href="https://github.com/srinkco/srink">
-                  <h5 class="mb-2 text-2xl font-semibold tracking-tight text-center pb-3">Srink</h5>
-              </a>
-              <p class="mb-3 font-normal text-gray-400 h-40">It is a robust URL shortener service developed in Go and backed by PostgreSQL. Designed for efficiency and scalability, Srink enables users to shorten long URLs into manageable links while maintaining reliable performance and data integrity.</p>
-              <a href="https://github.com/srinkco/srink" class="inline-flex font-medium items-center text-blue-600 hover:underline">
-                  Checkout now
-                  <svg class="w-3 h-3 ms-2.5 rtl:rotate-[270deg]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                      <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11v4.833A1.166 1.166 0 0 1 13.833 17H2.167A1.167 1.167 0 0 1 1 15.833V4.167A1.166 1.166 0 0 1 2.167 3h4.618m4.447-2H17v5.768M9.111 8.889l7.778-7.778"/>
-                  </svg>
-              </a>
-            </div>
-            <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-              <a href="https://github.com/celestix/skybox">
-                  <h5 class="mb-2 text-2xl font-semibold tracking-tight text-center pb-3">Skybox</h5>
-              </a>
-              <p class="mb-3 font-normal text-gray-400 h-40">It is a high-performance file server written in Rust, ensuring secure and efficient file storage and retrieval. It leverages Rust's memory safety and concurrency features for reliable and fast operations.</p>
-              <a href="https://github.com/celestix/skybox" class="inline-flex font-medium items-center text-blue-600 hover:underline">
-                  Checkout now
-                  <svg class="w-3 h-3 ms-2.5 rtl:rotate-[270deg]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                      <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11v4.833A1.166 1.166 0 0 1 13.833 17H2.167A1.167 1.167 0 0 1 1 15.833V4.167A1.166 1.166 0 0 1 2.167 3h4.618m4.447-2H17v5.768M9.111 8.889l7.778-7.778"/>
-                  </svg>
-              </a>
-            </div>
+          <div class="mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10 items-center justify-center">
+            <ProjectCard 
+                name="Galactron" 
+                description="A powerful Telegram bot to help people with channel management, written in Go." 
+                link="https://t.me/GalactronBot"
+            />
+            <ProjectCard 
+                name="GoTGProto" 
+                description="A package that aims to make td's raw functions easy-to-use with the help of features like using session strings, custom helper functions, storing peers and extracting chat or user ids through it etc. It is written in Go." 
+                link="https://github.com/celestix/gotgproto"
+            />
+            <ProjectCard 
+                name="WarpDL" 
+                description="WarpDL is a powerful and versatile cross-platform download manager. With its advanced technology, It has the ability to accelerate your download speeds by up to 10 times, revolutionizing the way you obtain files on any operating system." 
+                link="https://github.com/warpdl/warpdl"
+            />
+            <ProjectCard 
+                name="AgroConnect" 
+                description="AgroConnect is a one-place app that provides direct-to-consumer services such as buying and selling crops, cutting out the middleman, and more." 
+                link="https://github.com/celestix/agroconnect"
+            />
+            <ProjectCard 
+                name="Ferrisgram" 
+                description="FerrisGram is an asynchronous autogenerated wrapper for the Telegram Bot API written in Rust. This library provides all the methods and types that are available on the official Telegram Bot API." 
+                link="https://github.com/ferrisgram/ferrisgram"
+            />
+            <ProjectCard 
+                name="Srink" 
+                description="It is a robust URL shortener service developed in Go and backed by PostgreSQL. Designed for efficiency and scalability, Srink enables users to shorten long URLs into manageable links while maintaining reliable performance and data integrity." 
+                link="https://github.com/srinkco/srink"
+            />
+            <ProjectCard 
+                name="SkyBox" 
+                description="It is a high-performance file server written in Rust, ensuring secure and efficient file storage and retrieval. It leverages Rust's memory safety and concurrency features for reliable and fast operations." 
+                link="https://github.com/celestix/skybox"
+            />
           </div>
         </div>
     )
