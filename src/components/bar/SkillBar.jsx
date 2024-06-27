6dfe96038cc08e81ddb01c4cd0dc0819fccfd31b
celestix <i@anodite.me>
2024-06-27 17:19:05 +0530

    use grid for skill bar

--- src/components/bar/SkillBar.jsx
+++ src/components/bar/SkillBar.jsx
@@ -1,9 +1,9 @@
 export default function SkillBar({name, progress}) {
     return (
-        <div class="flex flex-row items-center space-x-4">
-            <p class="font-semibold w-36">{name}</p>
+        <div class="grid md:grid-rows-2 grid-cols-1 items-center md:space-y-0 space-y-3">
+            <p class="font-semibold">{name}</p>
             <div class="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
-            <div class="bg-blue-600 h-2.5 rounded-full" style={{width: progress}}></div>
+                <div class="bg-blue-600 h-2.5 rounded-full" style={{width: progress}}></div>
             </div>
         </div>
     )
