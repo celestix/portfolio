9ab118db031e834cfa724599020cbcb7faf2883d
celestix <i@anodite.me>
2024-06-27 16:22:15 +0530

    optimize navbar for mobile devices

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,22 +1,48 @@
 export default function NavBar() {
+    const navToggle = () => document.getElementById('nav-menu').classList.toggle('hidden');
     return (
-        <nav class="pt-5 overflow-hidden fixed w-full backdrop-blur-3xl">
-            <div class="flex md:flex-row flex-col justify-around text-white">
-                <div class="px-2 font-black font-sans text-center italic text-4xl">anodite.me</div>
-                <br class="md:hidden" />
-                <div class="px-10 space-x-20 flex md:flex-row flex-col text-2xl items-center">
-                    <div class="hover:text-gray-300 text-center">
-                        <a href="#home">Home</a>
+        <nav class="pt-5 overflow-hidden fixed w-full backdrop-blur-lg text-white">
+            <div class="md:px-20 md:flex md:justify-around items-center md:flex-row flex-col">
+                <div class="flex flex-row items-center justify-around">
+                    <div class="px-2 font-black font-sans text-center italic text-4xl justify-start">anodite.me</div>
+                    <div class="justify-end md:hidden" onClick={navToggle}>
+                        <svg class="w-6 h-6 text-gray-100 hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 17 14"><path d="M16 2H1a1 1 0 0 1 0-2h15a1 1 0 1 1 0 2Zm0 6H1a1 1 0 0 1 0-2h15a1 1 0 1 1 0 2Zm0 6H1a1 1 0 0 1 0-2h15a1 1 0 0 1 0 2Z"></path></svg>
                     </div>
-                    <div class="hover:text-gray-300">
-                        <a href="https://blog.anodite.me">Blog</a>
-                    </div>
-                    <div class="hover:text-gray-300">
-                        <a href="https://github.com/celestix">Github</a>
+                </div>
+                <div id="nav-menu" class="md:pt-0 md:w-auto w-full pt-5 mt-2 md:block hidden">
+                    <hr class="h-px mb-7 border-0 bg-gray-700 md:hidden" />
+                    <div class="px-10 md:flex-row md:space-x-20 flex-col md:text-2xl text-4xl items-center md:flex text-center">
+                        <div class="hover:text-gray-300">
+                            <a href="#home">Home</a>
+                        </div>
+                        <hr class="h-px my-7 border-0 bg-gray-700 md:hidden" />
+                        <div class="hover:text-gray-300">
+                            <a href="https://blog.anodite.me">Blog</a>
+                        </div>
+                        <hr class="h-px my-7 border-0 bg-gray-700 md:hidden" />
+                        <div class="hover:text-gray-300">
+                            <a href="https://github.com/celestix">Github</a>
+                        </div>
                     </div>
                 </div>
             </div>
             <hr class="h-px mt-7 border-0 bg-gray-700" />
+            {/* <div id="nav-menu" class="pt-5 mt-2 hidden">
+                <div class="px-10 md:flex-row md:space-x-20 flex-col text-4xl items-center md:flex text-center">
+                        <div class="hover:text-gray-300">
+                            <a href="#home">Home</a>
+                        </div>
+                        <hr class="h-px my-7 border-0 bg-gray-700" />
+                        <div class="hover:text-gray-300">
+                            <a href="https://blog.anodite.me">Blog</a>
+                        </div>
+                        <hr class="h-px my-7 border-0 bg-gray-700" />
+                        <div class="hover:text-gray-300">
+                            <a href="https://github.com/celestix">Github</a>
+                        </div>
+                </div>
+                <hr class="h-px mt-7 border-0 bg-gray-700" />
+            </div> */}
         </nav>
     )
 }
--- src/sections/home/Home.jsx
+++ src/sections/home/Home.jsx
@@ -1,13 +1,13 @@
 export default function Home() {
   return (
     <div class="flex justify-center items-center py-48">
-      <div class="left-content">
-        <div class="text-9xl font-extrabold">
-          <p class="py-8">Hello,</p>
+      <div class="xl:left-content">
+        <div class="min-[1640px]:text-9xl min-[1400px]:text-8xl md:text-7xl sm:text-6xl text-5xl font-extrabold">
+          <p class="pt-8 lg:pb-8 md:pb-6 pb-2">Hello,</p>
           <p class="animate-typing overflow-hidden whitespace-nowrap border-r-4 border-r-white">I am <span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Celestix</span></p>
         </div>
       </div>
-      <div class="right-content mx-36 rounded-lg overflow-hidden">
+      <div class="xl:right-content mx-36 rounded-lg overflow-hidden xl:block" hidden>
         <img src="https://avatars.githubusercontent.com/u/73958752" class="pointer-events-none" alt="github profile logo"/>
       </div>
     </div>
